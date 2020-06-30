7970c7723f1fd3a1dc0c77b1125e1e9da7896160
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-06-30 12:19:39 +0630

    replaced redux with context system

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,19 @@
 import React from "react";
 import UserCreate from "./UserCreate";
-import LanguageContext from "../context/LanguageContext";
 import ColorContext from "../context/ColorContext";
 import LanguageSelector from "./LanguageSelector";
+import { LanguageStore } from "../context/LanguageContext";
 
 class App extends React.Component {
-  state = {
-    language: "english",
-  };
-  onLanguageChange = (language) => {
-    this.setState({
-      language,
-    });
-  };
   render() {
     return (
       <div className="ui container">
-        <LanguageSelector onLanguageChange={this.onLanguageChange} />
-        <ColorContext.Provider value="primary">
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value="primary">
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
